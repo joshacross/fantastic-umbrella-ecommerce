7288bc8bb6db44ce602000a4996a7564d0565dff
Killa J <joshuaalexandercross@gmail.com>
2021-05-08 23:15:55 -0500

    created DB models and added category API routes

--- models/Category.js
+++ models/Category.js
@@ -6,8 +6,23 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-  },
+        // define column id
+        id: {
+          // id = number
+          type: DataTypes.INTEGER,
+          // not null
+          allowNull: false,
+          // instruct that this is the Primary Key
+          primaryKey: true,
+          // turn on auto increment
+          autoIncrement: true
+      },
+      // define username
+      category_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+      }
+    },
   {
     sequelize,
     timestamps: false,
